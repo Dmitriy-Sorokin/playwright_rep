f2c1ff97fc69e4c6ef160e3aec706814318f6034
Dmitriy Sorokin <sorokin.dm@promail366.com>
2023-05-22 19:54:43 +0300

    fixture

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+
+@pytest.fixture
+def browser_fixture():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        viewport_size = {"width": 1920, "height": 1080}
+        page.set_viewport_size(viewport_size=viewport_size)
+        page.goto("https://playwright-todomvc.antonzimaiev.repl.co/#/")
+        yield page
+        page.close()
+        browser.close()
