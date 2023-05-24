f0cb80f9ff8b559641c51c6251751de533e4d4ac
Dmitriy Sorokin <sorokin.dm@promail366.com>
2023-05-24 18:56:55 +0300

    assert

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,16 @@
 import pytest
 from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def browser_fixture():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
+        browser = playwright.chromium.launch(args=['--start-maximized'], headless=False)
+        context = browser.new_context(no_viewport=True)
         page = context.new_page()
-        viewport_size = {"width": 1920, "height": 1080}
-        page.set_viewport_size(viewport_size=viewport_size)
+        # viewport_size = {"width": 1920, "height": 1080}
+        # page.set_viewport_size(viewport_size=viewport_size)
         yield page
         page.close()
         browser.close()
