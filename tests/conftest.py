22696bf77c1e900053177ee8683e74f617c99fa7
Dmitriy Sorokin <sorokin.dm@promail366.com>
2023-05-25 16:10:52 +0300

    trase

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,11 @@ def browser_fixture():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(args=['--start-maximized'], headless=False)
         context = browser.new_context(no_viewport=True)
+        context.tracing.start(screenshots=True, snapshots=True, sources=True)
         page = context.new_page()
         # viewport_size = {"width": 1920, "height": 1080}
         # page.set_viewport_size(viewport_size=viewport_size)
         yield page
+        context.tracing.stop(path="trace.zip")
         page.close()
         browser.close()
